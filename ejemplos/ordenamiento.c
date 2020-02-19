da0fb8e1a47ef7a7572308bf632f99fd8610817e
Alejandro Ramirez <lunaticolupin@outlook.com>
2020-02-19 16:54:39 -0600

    Ordenamiento

---
+++ ejemplos/ordenamiento.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int arreglo[1000];
+
+void imprime(int arreglo[], int size){
+    for(int i=0; i<size; i++){
+        if (i==0){
+            printf("\n[%d,",arreglo[i]);
+            continue;
+        }
+
+        if (i==size-1){
+            printf("%d]\n",arreglo[i]);
+            continue;
+        }
+
+        printf("%d,", arreglo[i]);
+    }
+}
+
+void generar_elementos(int size){
+    srand((unsigned) time(NULL));
+
+    for(int i=0; i<size; i++){
+        arreglo[i]=rand()%1000;
+    }
+
+    imprime(arreglo,size);
+}
+
+int menor(int arreglo[], int size, int inicio){
+    int menor = arreglo[inicio];
+    int posicion = inicio;
+
+    for (int i = inicio; i<size; i++){
+        if (arreglo[i] < menor){
+            menor = arreglo[i];
+            posicion = i;
+        }
+    }
+
+    return posicion;
+}
+
+void burbuja(int arreglo[], int size){
+    for(int i = 0; i<size; i++){
+        for (int j = 0; j<size-1; j++){
+            if(arreglo[j] > arreglo[j+1]){
+                int temp = arreglo[j];
+                
+                arreglo[j] = arreglo[j+1];
+                arreglo[j+1] = temp;
+            }
+        }
+    }
+
+    imprime(arreglo,size);
+}
+
+void seleccion(int arreglo[], int size){
+    for (int i=0; i<size-1;i++){
+        int posicion_menor = menor(arreglo,size,i);
+        int temp = arreglo[i];
+
+        arreglo[i] = arreglo[posicion_menor];
+        arreglo[posicion_menor] = temp;
+    }
+
+    imprime(arreglo, size);
+}
+
+void insercion(int arreglo[], int size){
+    for (int i=1; i<size; i++){
+        int temp = arreglo[i];
+        int j = i-1;
+
+        while((arreglo[j]>temp && (j>=0))){
+            arreglo[j+1] = arreglo[j];
+            j--;
+        }
+
+        arreglo[j+1] = temp;
+    }
+}
+
+void rapido(int arreglo[], int inferior, int superior){
+    int pivote = arreglo[superior];
+    int temp;
+    int i = inferior -1, j = superior;
+    
+    if(inferior > superior)
+        return;
+    
+    while(1){
+        while (arreglo[++i] < pivote);
+        while (arreglo[--j] > pivote);
+
+        if (i<j){
+            temp = arreglo[i];
+            arreglo[i]=arreglo[j];
+            arreglo[j]=temp;
+        }else{
+            break;
+        }       
+    }
+
+    temp = arreglo[i];
+    arreglo[i] = arreglo[superior];
+    arreglo[superior] = temp;
+
+    rapido(arreglo, inferior, i-1);
+    rapido(arreglo,i+1, superior);
+}
+
+int main(){
+    
+    int n=0;
+
+    printf("Tamaño del arreglo: ");
+    scanf("%d",&n);
+
+    printf("Generar los elementos del arreglo");
+    generar_elementos(n);
+
+    printf("Ordenamiento Burbuja");
+    burbuja(arreglo,n);
+
+    imprime(arreglo,n);
+}
